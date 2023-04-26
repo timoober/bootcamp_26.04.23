14791b8ab67e0fcec600911943f0347c3cac1d23
timoober <timo.oberschachtsiek@gmail.com>
2023-04-26 16:31:12 +0200

    26.04.23 alle Bootcamp aufgaben

---
+++ assets/main.js
@@ -0,0 +1,46 @@
+// let varName = "Supercoder"
+
+// function intro2(paramName) {
+//     console.log("Hi, " + varName + ". Mein Name ist " +paramName + ".");
+// }
+
+// intro2 ("Timo")
+
+//! Lev1_3
+
+
+// let name = "SuperCoder"
+// let stadt = "Bielefeld"
+// let alter = "35"
+
+// function intro3(name, stadt, alter) {
+//     console.log("Hallo, mein Name ist " + name + "ich bin " + alter + "Jahre alt." + "Ich komme aus" + stadt);
+// 
+
+// intro3(name, stadt, alter)
+
+//! Lev1_5
+
+// function math(para1, para2) {
+//     console.log(para1 * para2);
+//     console.log(para1 / para2);
+// }
+
+// math (10, 2)
+
+//! Lev_1_2
+
+// document.getElementById("info").innerHTML = "<h1>Hello World</h1>" + "<h2>How are you</h2>"
+// // let info = document
+// console.log(info);
+
+// document.write ("end of the element") //!
+
+// document.getElementById('container').innerHTML = ("<p>start of the element</p>")
+
+
+//!
+// let gallery= document.getElementById('gallery')
+// gallery.innerHTML += "<div>  <figure><img src=https://unsplash.it/250%22%3E</figure> </div>"
+
+// console.log(gallery);
